882e0d35c8d4d3c32c35acd97887958497104be0
Steve Razzano <40343937+srazzano15@users.noreply.github.com>
2024-10-17 06:26:03 -0400

    Delete next.config.mjs from online repo, add to gitignore

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-   env: {
-      API_URL: "http://127.0.0.1:8000/api",
-      POLYGON_API_KEY: 'PCdoX9AUilgqttMkKOLjdSScnKHZ6J8M'
-   }
-};
-
-export default nextConfig;
