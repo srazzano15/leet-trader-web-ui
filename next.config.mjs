f5af591450d2590ed09c6fa19021e804459b24be
srazzano15 <srazzano15@gmail.com>
2024-10-17 19:04:44 -0400

    congif?

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
    env: {
       API_URL: "http://127.0.0.1:8000/api",
-      POLYGON_API_KEY: 'PCdoX9AUilgqttMkKOLjdSScnKHZ6J8M'
+      BASE_URL: "http://127.0.0.1:8000",
+      POLYGON_API_KEY: 'PCdoX9AUilgqttMkKOLjdSScnKHZ6J8M',
+      ALPACA_API_KEY_ID: 'PKXVKQTFW701A3PJAPBQ',
+      ALPACA_API_SECRET_KEY:'LfaD2SZnbjrZMhuGNrnMHU8zWFJGNBO2LuwghaEE',
    }
 };
 
